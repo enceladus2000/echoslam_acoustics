28cb710070914f1d43e918a5b14fbd025c7f10e9
Tanmay Bhonsale <tpb2k99@gmail.com>
2021-06-23 12:52:44 +0530

    made and loaded t_pipe from yaml

--- experiments/pipebots/t_pipe.py
+++ experiments/pipebots/t_pipe.py
@@ -1,37 +1,31 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pyroomacoustics as pra
-import time
+
+import sys
+sys.path.append('.')
+from utils.gen_utils import *
+from utils.pra_utils import *
+
+room_path = 'experiments/pipebots/data/t_pipe.yaml'
 
 def main():
-	fs = 16000
-
-	# define the polygonal floor of the pipe
-	pol = np.array([
-		[0,0], [10,0], [10,-7], [12,-7], [12,9], [10,9], [10,2], [0,2]
-	]).T
-	room_material = pra.Material(energy_absorption=0.5, scattering=None)
-
-	room = pra.Room.from_corners(
-		pol,
-		fs=fs,
-		max_order=5,
-		ray_tracing=True,
-		air_absorption=True,
-	)
-	# create 3D pipe by extruding it 
-	height = 2
-	room.extrude(height, materials=room_material)
+	# load room from yaml dump
+	room = load_room(room_path)
+	print('Room Details:')
+	print_room(room)
+	
+	fs = room.fs
 
 	# TODO: set ray tracing params
 
-	robot_x = 3
-	room.add_source([robot_x, 1, 1])
-	room.add_microphone([robot_x+0.1, 1, 1])
+	# TODO: Store source and mic pos in YAML file
+	robot_x = 1
+	room.add_source([robot_x, 0.15, 0.15])
+	room.add_microphone([robot_x+0.1, 0.15, 0.15])
 
-	# TODO: time the simulation
-	# room.image_source_model()
-	# room.ray_tracing()
+	room.image_source_model()
+	room.ray_tracing()
 	room.compute_rir()
 
 	# plot RIR according to distance of object reflecting
@@ -42,10 +36,8 @@ def main():
 	plt.show()
 
 	# show room
-	room.plot(img_order=5)
-	plt.gca().set_xlim3d(left=0, right=16)
-	plt.gca().set_ylim3d(bottom=-7, top=9)
-	plt.gca().set_zlim3d(bottom=-1, top=15)
+	room.plot(img_order=3)
+	fix_plt_axs(plt, [-3, 7], [-1, 9])
 	plt.show()
 
 if __name__ == "__main__":
