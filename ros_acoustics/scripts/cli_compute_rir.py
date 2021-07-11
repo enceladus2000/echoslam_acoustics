70736a1bc990743d82ec6220140be4c2f76ae732
enceladus2000 <tpb2k99@gmail.com>
2021-07-12 00:21:19 +0530

    added basic service and client scripts

--- ros_acoustics/scripts/cli_compute_rir.py
+++ ros_acoustics/scripts/cli_compute_rir.py
@@ -1,17 +1,54 @@
 #!/usr/bin/env python3
+from numpy.lib.index_tricks import RClass
 from ros_acoustics.srv import ComputeRIR
 
-class BasicRIRClient(Node):
-	pass
-	# def __init__(self):
-	#     super().__init__('basic_rir_client')
-	#     self.cli = self.create_client(ComputeRIR, 'compute_rir')
-		
-	#     while not self.cli.wait_for_service(timeout_sec=1.0):
-	#         self.get_logger().info('service unavailable, trying again...')
-		
-	#     self.req = ComputeRIR.Request()
+import rclpy
+from rclpy.node import Node
 
-	# def send_request(self):
-	#     self.setRequestSourcePos(1, 2, 0.5)
-	#     self.setRe
+class BasicRIRClient(Node):
+	
+	def __init__(self):
+		super().__init__('basic_rir_client')
+		self.cli = self.create_client(ComputeRIR, 'compute_rir')
+		
+		while not self.cli.wait_for_service(timeout_sec=1.0):
+			self.get_logger().info('service unavailable, trying again...')
+		
+		self.req = ComputeRIR.Request()
+
+	def send_request(self):
+		self.req.source_pos.x = 2.
+		self.req.source_pos.y = 2.5
+		self.req.source_pos.z = 1.
+		self.req.mic_pos.x = 2.5
+		self.req.mic_pos.y = 2.5
+		self.req.mic_pos.z = 1.
+
+		self.future = self.cli.call_async(self.req)
+
+def main(args=None):
+	rclpy.init(args=args)
+
+	rir_client = BasicRIRClient()
+	rir_client.send_request()
+
+	while rclpy.ok():
+		rclpy.spin_once(rir_client)
+
+		if rir_client.future.done():
+			try:
+				response = rir_client.future.result()
+			except Exception as e:
+				rir_client.get_logger().info(
+					'Service call failed %r' % (e,))
+			else:
+				rir_client.get_logger().info('Received response!')
+
+			break
+
+	rir_client.destroy_node()
+
+	rclpy.shutdown()
+
+if __name__ == "__main__":
+	main()
--- ros_acoustics/scripts/srv_compute_rir.py
+++ ros_acoustics/scripts/srv_compute_rir.py
@@ -13,6 +13,12 @@ class ComputeRIRService(Node):
         self.srv = self.create_service(ComputeRIR, 'compute_rir', self.compute_rir_callback)
 
     def compute_rir_callback(self, request, response):
+        room = pra.ShoeBox([5, 5, 2], fs=8000, max_order=1)
+        room.add_source([request.source_pos.x, request.source_pos.y, request.source_pos.z])
+        room.add_microphone([request.mic_pos.x, request.mic_pos.y, request.mic_pos.z])
+        room.compute_rir()
+
+        response.rir.array = list(room.rir[0][0])
         return response
     
 def main(args=None):
